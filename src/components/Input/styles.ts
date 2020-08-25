bc34af145c1b9d2ddf29244649f8524712ecff09
João Vítor Batistella <joaovitor.batistella.ifrs@gmail.com>
2020-08-25 19:49:31 -0300

    25/08

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -40,7 +40,7 @@ export const Container = styled.div<ContainerProps>`
     props.isFocused &&
     css`
       color: #ff9000;
-      border-color: #ff9900;
+      border-color: #ff9000;
     `}
 
   input {
